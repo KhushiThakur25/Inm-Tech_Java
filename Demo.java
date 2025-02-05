b5be59e443870c24696bef09ae9986f1dd754bda
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2025-02-05 16:43:39 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,27 @@
+class Demo {
+    public static void main(String[] args) {
+        int n = 15;
+        int studentNumber = 56;
+        System.out.println("Hello , this is n :" + n);
+        System.out.println(studentNumber);
+        // System.out.println(n + 10);
+        // n += 10;
+        // System.out.println(n);
+        // System.out.println("Post-increment : " + n++);
+        // System.out.println("Hello , this is n :" + n);
+        // System.out.println("Pre-increment : " + ++n);
+        // System.out.println();
+        // System.out.println("Hello , this is n :" + n);
+        // System.out.println();
+        // System.out.println("Post-decrement : " + n--);
+        // System.out.println("Hello , this is n :" + n);
+        // System.out.println("Pre-decrement : " + --n);
+
+        boolean a = true;
+        boolean b = false;
+        boolean c = true;
+        System.out.println(!b);
+
+    }
+
+}
